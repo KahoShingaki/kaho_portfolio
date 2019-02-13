d92436b20e24aeaf4dfb69742aa88a54ed418537
Kaho Shingaki <kahorine.imunida@gmail.com>
2019-02-13 18:53:03 +0900

    Alert, Badge, Breadcrumb, Button, Button group, Button toolbar, Card, Carousel, Collapse,Dropdownの追加

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  MAILGUN_DOMAIN: JSON.stringify(process.env.MAILGUN_DOMAIN)
+  MAILGUN_DOMAIN: JSON.stringify(process.env.MAILGUN_DOMAIN),
   MAILGUN_APIKEY: JSON.stringify(process.env.MAILGUN_APIKEY)
 }
